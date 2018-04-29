f49d15f74c252253092d7e1f8c8f5389db84c41e
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-04-30 11:48:44 +1200

    Create auth branch

---
+++ server/Routes/auth.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.post('/register', register)
+
+function register (req, res) {
+  const {username, password} = req.body
+  // TODO: make sure username doesn't already exist
+  // TODO: if not, hash the password and add the user to the database
+}
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -4,5 +4,6 @@ const express = require('express')
 const server = express()
 
 server.use(express.static(path.join(__dirname, './public')))
+server.use(express.json())
 
 module.exports = server
